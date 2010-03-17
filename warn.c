93d5e50dfc21cd5f8957bf5aa72c834efe1d9648
Randy Thelen <rthelen@madscientistroom.org>
2010-03-17 09:04:17 +0000

    Adding an unpredictable function for, mostly, cases where the PC is used incorrectly.  And added code to dump registers whenever a warning or unpredictable message is printed.  And, added a prefix string to the warnings and unpredictable messages.
    git-svn-id: file:///home/david/src/arm_sim_svn/trunk@35 d0763410-cf01-4c7c-9b55-0f7f3399ec47

--- warn.c
+++ warn.c
@@ -1,10 +1,26 @@
 #include "sim.h"
+#include "arm.h"
+
+void unpredictable(const char *fmt, ...)
+{
+    va_list ap;
+
+    printf("UNPREDICTABLE INSTRUCTION BEHAVIOR: ");
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    printf("\n");
+    arm_dump_registers();
+}
 
 void warn(const char *fmt, ...)
 {
     va_list ap;
 
+    printf("Warning: ");
     va_start(ap, fmt);
     vprintf(fmt, ap);
     va_end(ap);
+    printf("\n");
+    arm_dump_registers();
 }
